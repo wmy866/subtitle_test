f2407eb7084999762ca8ba5dca81c61d0933390c
wmy866 <71766802+wmy866@users.noreply.github.com>
2026-08-20 12:53:21 +0800

    Update YouTube video ID in video_script.js

--- video_script.js
+++ video_script.js
@@ -2,6 +2,6 @@ let player;  //定義變數用來存放YT播放器
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player("player",{  //"player"是HTML中id為player的容器
-        videoId: "mMg0PV3FAfE"  //影片ID
+        videoId: "4WybwsKIeIQ"  //影片ID
     });
 }
