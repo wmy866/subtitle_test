578d128f3167b8f74cff114ad8de1f5ecfd4dc1d
wmy866 <71766802+wmy866@users.noreply.github.com>
2026-08-20 12:46:55 +0800

    Add files via upload

--- video_script.js
+++ video_script.js
@@ -2,8 +2,6 @@ let player;  //定義變數用來存放YT播放器
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player("player",{  //"player"是HTML中id為player的容器
-        width: "640",
-        height: "360",
-        videoId: "4WybwsKIeIQ"  //影片ID
+        videoId: "mMg0PV3FAfE"  //影片ID
     });
 }
