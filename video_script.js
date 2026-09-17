31632fb3ec5c30b6548406da78714529d5c9bb1d
wmy866 <71766802+wmy866@users.noreply.github.com>
2026-09-17 16:14:46 +0800

    Delete video_script.js

--- video_script.js
+++
@@ -1,7 +0,0 @@
-let player;  //定義變數用來存放YT播放器
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player("player",{  //"player"是HTML中id為player的容器
-        videoId: "4WybwsKIeIQ"  //影片ID
-    });
-}
